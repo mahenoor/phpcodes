9fa943e8045e31775908449e207bc275135248df
mahenoor <mahe.noor@compassitesinc.com>
2016-10-19 16:00:36 +0530

    Delete pdo4.php

--- pdo4.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'bootstrap.php';
-require 'Task.php';
-$pdo   = connection::make();
-$query = new QueryBuilder($pdo);
-$tasks = $query->selectAll('today');
-die(var_dump($tasks));
-?> 
